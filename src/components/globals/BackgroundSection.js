659566f04c22fa6b4800575e688c601582a2a4ea
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-24 00:53:43 -0400

    need to fix linear gradient

--- src/components/globals/BackgroundSection.js
+++ src/components/globals/BackgroundSection.js
@@ -1,11 +1,25 @@
 import React from "react"
 import BackgroundImage from "gatsby-background-image"
 
+// wrapper around background image allows us reuse any props that we give BackgroundImage
+
 export default function BackgroundSection({
   img,
   styleClass,
   title,
   children,
 }) {
-  return <BackgroundImage
+  return (
+    <BackgroundImage className={styleClass} fluid={img}>
+      <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
+        {title}
+      </h1>
+      {children}
+    </BackgroundImage>
+  )
+}
+
+BackgroundSection.defaultProps = {
+  title: "default title",
+  styleClass: "default-background",
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,20 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
+import BackgroundSection from "../components/globals/BackgroundSection"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello Dog Lovers!</h1>
-    <p>Are you ready to meet your forever friend ?</p>
-    <FaGulp />
+
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    />
   </Layout>
 )
 
+// querying our background image for us
 export const query = graphql`
   {
     img: file(relativePath: { eq: "default-background.jpeg" }) {
