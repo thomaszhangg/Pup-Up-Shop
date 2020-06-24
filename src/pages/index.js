c011728fc60ca67973a227d4732cade4e4ff8a6d
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-23 23:58:00 -0400

    setting bckg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaGulp } from "react-icons/fa"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello Dog Lovers!</h1>
@@ -13,5 +13,17 @@ const IndexPage = () => (
     <FaGulp />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
