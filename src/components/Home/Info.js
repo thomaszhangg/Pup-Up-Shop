12dd9c2914c14ac91427a70db0098082d156e5f7
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-25 02:21:38 -0400

    pworking on info component, filler text

---
+++ src/components/Home/Info.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              voluptate labore alias natus vel deleniti tempore, aut totam
+              veniam omnis recusandae minima quam ex? Numquam libero asperiores
+              nulla illo cupiditate deleniti voluptate a ut accusamus. Soluta
+              aliquam iste possimus! Eveniet neque corrupti mollitia quia
+              quisquam itaque dolorum repudiandae? Necessitatibus, ullam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
