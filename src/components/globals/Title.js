d4656df31e92b3f727f191ca48745dfb1b336173
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-25 02:37:28 -0400

    done with info section

--- src/components/globals/Title.js
+++ src/components/globals/Title.js
@@ -2,8 +2,10 @@ import React from "react"
 
 export default function Title({ title }) {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="row">
+      <div className="col text-center mb-3">
+        <h1 className="dispaly-4 text-uppercase font-weight-bold">{title}</h1>
+      </div>
     </div>
   )
 }
