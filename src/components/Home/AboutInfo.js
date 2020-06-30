44f84c3a48457b70c5745f112956807ee8f6a7d4
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-29 23:37:28 -0400

    description

--- src/components/Home/AboutInfo.js
+++ src/components/Home/AboutInfo.js
@@ -10,21 +10,7 @@ export default function Info() {
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
-              Welcome to Pup-Up Shop! 20 years ago, Thomas Zhang created
-              Greenfield Puppies with the intent to connect healthy puppies with
-              caring families. Pup-Up shop does not condone Puppy Mills, and we
-              strongly discourage against all breeding practices in which the
-              health and well-being of innocent dogs and puppies are
-              compromised. compromised. We run through an extensive and thorough
-              process to ensure you find a quality breeder through our website.
-              At Pup-Up Shop, we understand the importance of having your new
-              family member come to you a healthy, happy pup with good genetics.
-              We ensure this by offering a 30-day health guarantee provided by
-              every breeder. The adoption of a puppy is a big commitment. As a
-              prospective dog owner, we suggest that you thoroughly research the
-              breeds you are interested in. Feel free to email us at
-              thomaszhang97@gmail.com or use our contact form with any
-              questions.
+             
             </p>
             <Link to="/about/">
               <button className="btn text-uppercase btn-yellow">
