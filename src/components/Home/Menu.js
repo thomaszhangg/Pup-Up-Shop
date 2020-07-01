530eb70054b8af3c4f05ab4bcdbdb2e26b92dbf4
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-01 03:37:15 -0400

    graphql query for puppy items successful

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Title from '../Globals/Title'
+import Img from 'gatsby-image'
+
+export default class Menu extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props.items)
+    }
+
+    render() {
+        return (
+            <div>
+                Hola content from menu component
+            </div>
+        )
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -14,16 +15,36 @@ const IndexPage = ({ data }) => (
       title="Pup-up Shop"
     />
     <Info />
+    <Menu items={data.menu}/>
   </Layout>
 )
 
-// querying our background image for us
+// querying our background image and puppyItems from contentful
+// replace src fragment with gatsby-source-contentful
 export const query = graphql`
   {
     img: file(relativePath: { eq: "default-background.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+
+    menu: allContentfulPuppyItem {
+      edges {
+        node {
+          id
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 70, height: 70) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
