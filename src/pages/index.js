cae90b3f096cab1e8dd04c07c1f658e0cf0801e9
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-02 03:43:46 -0400

    removed _tracedSVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 70, height: 70) {
-              ...GatsbyContentfulFixed_tracedSVG
+              ...GatsbyContentfulFixed
             }
           }
         }
