1def4ee3dc75cfa8315556fe90c1b5b37991b8cf
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-03 02:00:16 -0400

    added puppies to product section, need to resize cards

--- src/components/Home/Puppies.js
+++ src/components/Home/Puppies.js
@@ -3,7 +3,7 @@ import Puppy from "./Puppy"
 import Title from "../Globals/Title"
 import { StaticQuery, graphql } from "gatsby"
 
-const getProducts = graphql`
+const getPuppies = graphql`
   {
     puppies: allContentfulPuppy {
       edges {
@@ -23,12 +23,23 @@ const getProducts = graphql`
   }
 `
 
-export default function Products() {
+export default function Puppies() {
   return (
     <StaticQuery
-      query={getProducts}
+      query={getPuppies}
       render={data => {
-        return <h1>Hello World</h1>
+        return (
+          <section className="py-5">
+            <div className="container">
+              <Title title="Featured Puppies" />
+              <div className="row">
+                {data.puppies.edges.map(({ node: puppy }) => {
+                  return <Puppy key={puppy.id} puppy={puppy} />
+                })}
+              </div>
+            </div>
+          </section>
+        )
       }}
     />
   )
--- src/components/Home/Puppy.js
+++ src/components/Home/Puppy.js
@@ -1,5 +1,20 @@
 import React from "react"
+import Img from "gatsby-image"
 
-export default function Product() {
-  return <div>Hola from product</div>
+export default function Puppy({ puppy }) {
+  return (
+    <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
+      <div className="card" style={{ minHeight: "100%" }}>
+        <Img fluid={puppy.image.fluid} className="card-img-top" />
+
+        <div className="card-body text-center">
+          <h6>{puppy.title}</h6>
+          <h6>${puppy.price}</h6>
+          <button className="btn btn-yellow mt-3 text-capitalize">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
