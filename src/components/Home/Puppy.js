82dd7581e94b3d757d1b6a4e4f3f71c6a0682d25
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-03 03:04:43 -0400

    all images are resized properly - canva

--- src/components/Home/Puppy.js
+++ src/components/Home/Puppy.js
@@ -4,7 +4,11 @@ import Img from "gatsby-image"
 export default function Puppy({ puppy }) {
   return (
     <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
-      <div className="card" style={{ minHeight: "100%" }}>
+      <div
+        className="card"
+        style={{ minHeight: "100%" }}
+        imgStyle={{ objectFit: "contain" }}
+      >
         <Img fluid={puppy.image.fluid} className="card-img-top" />
 
         <div className="card-body text-center">
