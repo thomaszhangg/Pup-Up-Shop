a034198222bbb73f6d4f632dba848b791259d8fd
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-03 01:33:53 -0400

    changed imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
 import Menu from "../components/Home/Menu"
-import Products from "../components/Home/Products"
+import Puppies from "../components/Home/Puppies"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -17,7 +17,7 @@ const IndexPage = ({ data }) => (
     />
     <Info />
     <Menu items={data.menu} />
-    <Products />
+    <Puppies />
   </Layout>
 )
 
