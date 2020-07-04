8f6e5df1b75401454cae3e5279ba78a9a3a058d9
Thomas Zhang <thomaszhang97@gmail.com>
2020-07-04 03:38:06 -0400

    contact form, bs control

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,19 +1,50 @@
 import React from "react"
-import Title from '../Globals/Title'
-
+import Title from "../Globals/Title"
 
 export default function Contact() {
   return (
     <section className="contact py-5">
-        <Title title="contact us"/>
-        <div className="row">
-            {/* 10 cols wide xs ,s 8, med 6 */}
-            <div className="col-10 col-sm-8 col-md-6 mx-auto">
-                <form>
-                    
-                </form>
+      <Title title="contact us" />
+      <div className="row">
+        {/* 10 cols wide xs ,s 8, med 6 */}
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form>
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="Theomas Zhang"
+              />
             </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                email="email"
+                id="email"
+                placeholder="theomas@dog.com"
+              />
+            </div>
+            {/* description */}
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                id="description"
+                className="form-control"
+                rows="5"
+                placeholder="Questions or Concerns"
+              ></textarea>
+            </div>
+          </form>
         </div>
+      </div>
     </section>
   )
 }
